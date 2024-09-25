123903157fbd18b5fbbde8e1d11062042ba55298
flora-hang <147212289+flora-hang@users.noreply.github.com>
2024-09-24 21:50:46 -0700

    yarn prettier fix ran, nothing else changed

--- src/models/Course.ts
+++ src/models/Course.ts
@@ -1,26 +1,25 @@
-import Section from './Section';
+import Section from "./Section";
 
 export default class Course {
-    private readonly Subject: string; // department of the course, e.g. CPSC
-    private readonly NumSections: number; // number of sections in this course 
-    private readonly Sections: Section[];
+	private readonly Subject: string; // department of the course, e.g. CPSC
+	private readonly NumSections: number; // number of sections in this course
+	private readonly Sections: Section[];
 
-    constructor(subj: string, numS: number, sections: Section[]) {
-        this.Subject = subj;
-        this.NumSections = numS;
-        this.Sections = sections;
-    }
+	constructor(subj: string, numS: number, sections: Section[]) {
+		this.Subject = subj;
+		this.NumSections = numS;
+		this.Sections = sections;
+	}
 
-    public getSubject(): string {
-        return this.Subject;
-    }
+	public getSubject(): string {
+		return this.Subject;
+	}
 
-    public getNumSections(): number {
-        return this.NumSections;
-    }
+	public getNumSections(): number {
+		return this.NumSections;
+	}
 
-    public getSections(): Section[] {
-        return this.Sections;
-    }
-
+	public getSections(): Section[] {
+		return this.Sections;
+	}
 }
--- src/models/Dataset.ts
+++ src/models/Dataset.ts
@@ -1,25 +1,25 @@
-import Course from './Course';
+import Course from "./Course";
 
 export default class Dataset {
-    private readonly ID: string;
-    private totalSections: number;
-    private Courses: Course[];
+	private readonly ID: string;
+	private totalSections: number;
+	private Courses: Course[];
 
-    constructor(id: string, total: number, courses: Course[]) {
-        this.ID = id;
-        this.totalSections = 0;
-        this.Courses = courses;
-    }
+	constructor(id: string, total: number, courses: Course[]) {
+		this.ID = id;
+		this.totalSections = 0;
+		this.Courses = courses;
+	}
 
-    public getId(): string {
-        return this.ID;
-    }
+	public getId(): string {
+		return this.ID;
+	}
 
-    public getTotalSections(): number {
-        return this.totalSections;
-    }
-    
-    public getCourses(): Course[] {
-        return this.Courses;
-    }
+	public getTotalSections(): number {
+		return this.totalSections;
+	}
+
+	public getCourses(): Course[] {
+		return this.Courses;
+	}
 }
