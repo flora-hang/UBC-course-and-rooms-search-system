6daea868e3ffe5df4cfedc287b23a5b676dd6755
Zenia Lau <zenialaulau.lau@gmail.com>
2024-09-24 21:13:31 -0700

    set up Section.ts

---
+++ src/models/Section.ts
@@ -0,0 +1,65 @@
+export default class Section {
+    private readonly uuid: string; // "id" A identifier for the section
+    private readonly id: string; // "Course" The course identifier
+    private readonly title: string; // "Title" The name of the course
+    private readonly instructor: string; // "Professor" The name fo the instructor who taught the section
+    private readonly dept: string; // "Subject" The department which offered the section
+    private readonly year: number; // "Year" The year in which the section was run. Set to 1900 when Section = overall
+    private readonly avg: number; // "Avg" The average grade received by students in section
+    private readonly pass: number; // "Pass" The number of students who passed in the section
+    private readonly fail: number; // "Fail" The number of students who failed in the section
+    private readonly audit: number; // "Audit" The number of students who audited the section
+
+    constructor(uuid: string, id: string, title: string, instructor: string, dept: string, year: number, avg: number, pass: number, fail: number, audit: number) {
+        this.uuid = uuid;
+        this.id = id;
+        this.title = title;
+        this.instructor = instructor;
+        this.dept = dept;
+        this.year = year;
+        this.avg = avg;
+        this.pass = pass;
+        this.fail = fail;
+        this.audit = audit;
+    }
+
+    public getUuid(): string {
+        return this.uuid;
+    }
+
+    public getId(): string {
+        return this.id;
+    }
+
+    public getTitle(): string {
+        return this.title;
+    }
+
+    public getInstructor(): string {
+        return this.instructor;
+    }
+
+    public getDept(): string {
+        return this.dept;
+    }
+
+    public getYear(): number {
+        return this.year;
+    }
+
+    public getAvg(): number {
+        return this.avg;
+    }
+
+    public getPass(): number {
+        return this.pass;
+    }
+
+    public getFail(): number {
+        return this.fail;
+    }
+
+    public getAudit(): number {
+        return this.audit;
+    }
+}
