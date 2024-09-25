7bf80460eac4311c235ae51d6dc619e1037426f1
flora-hang <147212289+flora-hang@users.noreply.github.com>
2024-09-24 21:47:33 -0700

    Course class and Dataset class added under src/model folder

---
+++ src/models/Course.ts
@@ -0,0 +1,26 @@
+import Section from './Section';
+
+export default class Course {
+    private readonly Subject: string; // department of the course, e.g. CPSC
+    private readonly NumSections: number; // number of sections in this course 
+    private readonly Sections: Section[];
+
+    constructor(subj: string, numS: number, sections: Section[]) {
+        this.Subject = subj;
+        this.NumSections = numS;
+        this.Sections = sections;
+    }
+
+    public getSubject(): string {
+        return this.Subject;
+    }
+
+    public getNumSections(): number {
+        return this.NumSections;
+    }
+
+    public getSections(): Section[] {
+        return this.Sections;
+    }
+
+}
---
+++ src/models/Dataset.ts
@@ -0,0 +1,25 @@
+import Course from './Course';
+
+export default class Dataset {
+    private readonly ID: string;
+    private totalSections: number;
+    private Courses: Course[];
+
+    constructor(id: string, total: number, courses: Course[]) {
+        this.ID = id;
+        this.totalSections = 0;
+        this.Courses = courses;
+    }
+
+    public getId(): string {
+        return this.ID;
+    }
+
+    public getTotalSections(): number {
+        return this.totalSections;
+    }
+    
+    public getCourses(): Course[] {
+        return this.Courses;
+    }
+}
