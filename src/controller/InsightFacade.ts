b67dc1ae3fd6369a3266260be23b63581fea21a2
flora-hang <147212289+flora-hang@users.noreply.github.com>
2024-10-02 10:30:21 -0700

    try splitting reading

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -196,9 +196,11 @@ export default class InsightFacade implements IInsightFacade {
 				// const course = new Course(courseName);
 				// courses.push(course);
 				courses.push(new Course(courseName));
-				const prom = void zip.files[filename].async("string");
+				// const prom = void zip.files[filename].async("string");
+				const f = zip.files[filename];
+				proms.push(f.async("string"));
 				// proms.push(zip.files[filename].async("string"));
-				proms.push(prom);
+				//proms.push(prom);
 			});
 			const array = await Promise.all(proms); // Wait for all files to be processed
 
